b9977bb526da54b90794e6b4690d80f213b23960
saurabhgupta0527@gmail.com <saurabhgupta0527@gmail.com@e8411c8e-9d7c-c8cd-2363-7bd78e0e9965>
2013-01-30 16:50:38 +0000

    CAMS_Initialize.cpp

---
+++ trunk/cams/src/CAMS_Initialize.cpp
@@ -0,0 +1,81 @@
+/******************************************************************************/
+// File Name		:	CAMS_Initialize.h
+//
+// Author			:	
+//
+// Project			:	CAMS FRAMEWORK
+//
+// Source			:	CAMS re-usable modules
+//
+// Module			:	CAMS AGENT
+//
+// Purpose			:	use this class as a friend to initialize the required modules.
+//
+// Copy Right		:	
+//
+// Notes			:
+//
+// History			:
+//		Created		:   02/07/2012
+//		Version		:	1.0.0.0
+//
+//
+//
+/******************************************************************************/
+#include "Log.h"
+#include "IdGlobals.h"
+#include "CAMSDBUtil.h"
+#include "CAMSGlobal.h"
+#include "CAMS_Initialize.h"
+
+#include "IdXmlWkr.h"
+#include "IdXmlUtility.h"
+
+#include <iostream>
+
+extern class CLog g_tLog;
+extern CCAMSDBUtil g_dbu;
+extern CCAMS_Initialize g_inlz;
+
+using namespace std;
+
+CCAMS_Initialize :: CCAMS_Initialize ()
+{
+}
+
+CCAMS_Initialize :: ~CCAMS_Initialize ()
+{
+}
+
+void CCAMS_Initialize :: m_initialize_logger ()
+{
+	g_tLog.Open("../../log/", false);
+}
+
+void CCAMS_Initialize :: m_initialize_config_settings ()
+{
+    g_tLog.WriteLog ("ENTER : CCAMS_Initialize :: m_initialize_config_settings");
+
+	CIdXmlUtility xmlu;
+
+    g_tLog.WriteLog ("Opening Configuration Settings file <%s>", CAMS_CONFIG_SETTINGS);
+
+	xmlu.OpenXMLFile (CAMS_CONFIG_SETTINGS);
+
+    xmlu.GetValue (CONN_DATABASE, g_dbu.m_strDataBase);
+    g_tLog.WriteLog ("Found database = %s", (g_dbu.m_strDataBase).c_str());
+
+    xmlu.GetValue (CONN_SERVER, g_dbu.m_strServer);
+    g_tLog.WriteLog ("Found server = %s", (g_dbu.m_strServer).c_str());
+
+    xmlu.GetValue (CONN_PORT, g_dbu.m_strPort);
+    g_tLog.WriteLog ("Found port = %s", (g_dbu.m_strPort).c_str());
+
+    xmlu.GetValue (CONN_UID, g_dbu.m_strUID);
+    g_tLog.WriteLog ("Found uid = %s", (g_dbu.m_strUID).c_str());
+
+    xmlu.GetValue (CONN_PASSWORD, g_dbu.m_strPWD);
+    g_tLog.WriteLog ("Found password, skipping print for security reason");
+
+    g_tLog.WriteLog ("ENTER : CCAMS_Initialize :: m_initialize_config_settings");
+}
